2eeb7f3d8983e67a976d4521cc59d55fc2b36998
mbrightbill <brightbill.mt@gmail.com>
2015-03-28 18:09:30 -0700

    Added Table View and nav controller in TodayVC

--- Agenda/TodayViewController.swift
+++ Agenda/TodayViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class TodayViewController: UIViewController {
 
+    @IBOutlet weak var todayTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+  
 
 }
