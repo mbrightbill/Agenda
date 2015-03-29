808a6519d0d5c79bce0d5a05b309d5dd7a9a8451
mbrightbill <brightbill.mt@gmail.com>
2015-03-28 18:43:54 -0700

    Added alert controller with 2 actions and "add" selector method. Along with add button as right bar button

--- Agenda/TodayViewController.swift
+++ Agenda/TodayViewController.swift
@@ -8,14 +8,63 @@
 
 import UIKit
 
-class TodayViewController: UIViewController {
+class TodayViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var todayTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.todayTableView.delegate = self
+        self.todayTableView.dataSource = self
 
+        let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "addButtonPressed")
+        self.navigationItem.rightBarButtonItem = addButton
+        
     }
+    
+    // MARK: - UITableView delegate methods
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("TASK_CELL", forIndexPath: indexPath) as UITableViewCell
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // MARK: - Helper and selector methods
+    
+    func saveTask(task: String) {
+        
+        
+        
+    }
+    
+    func addButtonPressed() {
+        println("Compose button pressed")
+        
+        let alert = UIAlertController(title: "Agenda", message: "What will you accomplish today?", preferredStyle: UIAlertControllerStyle.Alert)
+        let addAction = UIAlertAction(title: "Add", style: UIAlertActionStyle.Default) { (action: UIAlertAction!) -> Void in
+            println("addAction pressed!")
+            
+            let textField = alert.textFields![0] as UITextField
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        
+        alert.addTextFieldWithConfigurationHandler { (textField: UITextField!) -> Void in
+        }
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Memory warning method
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
